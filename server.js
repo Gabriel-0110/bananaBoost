1aba20cf84582e9b906bbe8a97d2cb1373d602a5
Gabriel Chiappa <Gabriel@GGUnifiedTech.com>
2025-12-04 03:56:10 -0500

    Update static file serving and index.html path

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Serve static files from the Angular app dist directory
-app.use(express.static(path.join(__dirname, 'dist/nano-banana-pro/browser')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // Send all requests to index.html
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/nano-banana-pro/browser/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 app.listen(PORT, () => {
